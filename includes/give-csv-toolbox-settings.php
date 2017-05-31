5c2b1c6b56958c9c6d0372d93e35c5e2e9b97971
Peter Wise <peter@squarecandy.net>
2017-05-31 18:54:09 -0400

    cleanup whitespace

--- includes/give-csv-toolbox-settings.php
+++ includes/give-csv-toolbox-settings.php
@@ -79,16 +79,16 @@ if ( ! class_exists( 'Give_Settings_CSV' ) ) :
 		function view_toolbox() {
 			?>
 
-            <div class="give-csv-toolbox">
-                <div class="give-csv-toolbox-title-wrap">
-                    <h3><span><?php esc_html_e( 'Export Donation History and Custom Fields to CSV', 'give' ); ?></span>
-                    </h3>
-                    <p><?php _e( 'Download an export of donors for specific donation forms with the option to include custom fields.', 'give' ); ?></p>
-                </div>
-                <td>
-                    <form method="post" id="give-csv-toolbox" class="give-export-form">
-                        <div id="give-csv-toolbox-form-select-wrap">
-                            <h4><?php esc_html_e( 'Select a Donation Form:', 'give' ); ?></h4>
+			<div class="give-csv-toolbox">
+				<div class="give-csv-toolbox-title-wrap">
+					<h3><span><?php esc_html_e( 'Export Donation History and Custom Fields to CSV', 'give' ); ?></span>
+					</h3>
+					<p><?php _e( 'Download an export of donors for specific donation forms with the option to include custom fields.', 'give' ); ?></p>
+				</div>
+				<td>
+					<form method="post" id="give-csv-toolbox" class="give-export-form">
+						<div id="give-csv-toolbox-form-select-wrap">
+							<h4><?php esc_html_e( 'Select a Donation Form:', 'give' ); ?></h4>
 							<?php
 							$args = array(
 								'name'   => 'forms',
@@ -97,13 +97,13 @@ if ( ! class_exists( 'Give_Settings_CSV' ) ) :
 								'number' => - 1,
 							);
 							echo Give()->html->forms_dropdown( $args ); ?>
-                        </div>
+						</div>
 
-                        <div id="give-csv-toolbox-export-options">
+						<div id="give-csv-toolbox-export-options">
 
-                            <div id="give-csv-toolbox-export-date">
+							<div id="give-csv-toolbox-export-date">
 
-                                <h4><?php esc_html_e( 'Filter by Date:', 'give' ); ?></h4>
+								<h4><?php esc_html_e( 'Filter by Date:', 'give' ); ?></h4>
 								<?php
 								$args = array(
 									'id'          => 'give-payment-export-start',
@@ -118,183 +118,183 @@ if ( ! class_exists( 'Give_Settings_CSV' ) ) :
 									'placeholder' => esc_attr__( 'End date', 'give' ),
 								);
 								echo Give()->html->date_field( $args ); ?>
-                            </div>
+							</div>
 
-                            <div id="give-csv-toolbox-status">
+							<div id="give-csv-toolbox-status">
 
-                                <h4><?php esc_html_e( 'Filter by Status:', 'give' ); ?></h4>
-                                <select name="status">
-                                    <option value="any"><?php esc_html_e( 'All Statuses', 'give' ); ?></option>
+								<h4><?php esc_html_e( 'Filter by Status:', 'give' ); ?></h4>
+								<select name="status">
+									<option value="any"><?php esc_html_e( 'All Statuses', 'give' ); ?></option>
 									<?php
 									$statuses = give_get_payment_statuses();
 									foreach ( $statuses as $status => $label ) {
 										echo '<option value="' . $status . '">' . $label . '</option>';
 									}
 									?>
-                                </select>
+								</select>
 
-                            </div>
+							</div>
 
-                        </div>
+						</div>
 						<?php wp_nonce_field( 'give_ajax_export', 'give_ajax_export' ); ?>
 
 
-                        <div id="give-csv-toolbox-standard-fields" class="give-clearfix">
-                            <h4><?php esc_html_e( 'Standard Columns:', 'give' ); ?></h4>
-                            <ul id="give-export-option-ul">
-                                <li>
-                                    <label for="give-export-donation-id">
-                                        <input type="checkbox" checked
-                                               name="give_csv_toolbox_export_option[donation_id]"
-                                               id="give-export-donation-id"><?php esc_html_e( 'Donation ID', 'give' ); ?>
-                                    </label>
-                                </li>
-                                <li>
-                                    <label for="give-export-first-name">
-                                        <input type="checkbox" checked
-                                               name="give_csv_toolbox_export_option[first_name]"
-                                               id="give-export-first-name"><?php esc_html_e( 'Donor\'s First Name', 'give' ); ?>
-                                    </label>
-                                </li>
-                                <li>
-                                    <label for="give-export-last-name">
-                                        <input type="checkbox" checked
-                                               name="give_csv_toolbox_export_option[last_name]"
-                                               id="give-export-last-name"><?php esc_html_e( 'Donor\'s Last Name', 'give' ); ?>
-                                    </label>
-                                </li>
-                                <li>
-                                    <label for="give-export-email">
-                                        <input type="checkbox" checked
-                                               name="give_csv_toolbox_export_option[email]"
-                                               id="give-export-email"><?php esc_html_e( 'Donor\'s Email', 'give' ); ?>
-                                    </label>
-                                </li>
-                                <li>
-                                    <label for="give-export-address">
-                                        <input type="checkbox" checked
-                                               name="give_csv_toolbox_export_option[address]"
-                                               id="give-export-address"><?php esc_html_e( 'Donor\'s Billing Address', 'give' ); ?>
-                                    </label>
-                                </li>
-                                <li>
-                                    <label for="give-export-donation-sum">
-                                        <input type="checkbox" checked
-                                               name="give_csv_toolbox_export_option[donation_total]"
-                                               id="give-export-donation-sum"><?php esc_html_e( 'Donation Total', 'give' ); ?>
-                                    </label>
-                                </li>
-                                <li>
-                                    <label for="give-export-donation-status">
-                                        <input type="checkbox" checked
-                                               name="give_csv_toolbox_export_option[donation_status]"
-                                               id="give-export-donation-status"><?php esc_html_e( 'Donation Status', 'give' ); ?>
-                                    </label>
-                                </li>
-																<!-- Add Custom Fields -->
-																<li>
-																	<label for="give-export-give-simple-note-field">
-																		<input type="checkbox" checked
-																			name="give_csv_toolbox_export_option[give_simple_note_field]"
-																			id="give-export-give-simple-note-field"><?php esc_html_e( 'Note', 'give' ); ?>
-																	</label>
-																</li>
-																<li>
-																	<label for="give-export-give-simple-anon-field">
-																		<input type="checkbox" checked
-																			name="give_csv_toolbox_export_option[give_simple_anon_field]"
-																			id="give-export-give-simple-anon-field"><?php esc_html_e( 'Anonymous', 'give' ); ?>
-																	</label>
-																</li>
-																<!-- END Add Custom Fields -->
-                                <li>
-                                    <label for="give-export-payment-gateway">
-                                        <input type="checkbox" checked
-                                               name="give_csv_toolbox_export_option[payment_gateway]"
-                                               id="give-export-payment-gateway"><?php esc_html_e( 'Payment Gateway', 'give' ); ?>
-                                    </label>
-                                </li>
-                                <li>
-                                    <label for="give-export-donation-form-id">
-                                        <input type="checkbox" checked
-                                               name="give_csv_toolbox_export_option[form_id]"
-                                               id="give-export-donation-form-id"><?php esc_html_e( 'Donation Form ID', 'give' ); ?>
-                                    </label>
-                                </li>
-                                <li>
-                                    <label for="give-export-donation-form-title">
-                                        <input type="checkbox" checked
-                                               name="give_csv_toolbox_export_option[form_title]"
-                                               id="give-export-donation-form-title"><?php esc_html_e( 'Donation Form Title', 'give' ); ?>
-                                    </label>
-                                </li>
-                                <li>
-                                    <label for="give-export-donation-form-level-id">
-                                        <input type="checkbox" checked
-                                               name="give_csv_toolbox_export_option[form_level_id]"
-                                               id="give-export-donation-form-level-id"><?php esc_html_e( 'Donation Form Level ID', 'give' ); ?>
-                                    </label>
-                                </li>
-                                <li>
-                                    <label for="give-export-donation-form-level-title">
-                                        <input type="checkbox" checked
-                                               name="give_csv_toolbox_export_option[form_level_title]"
-                                               id="give-export-donation-form-level-title"><?php esc_html_e( 'Donation Form Level Title', 'give' ); ?>
-                                    </label>
-                                </li>
-                                <li>
-                                    <label for="give-export-donation-date">
-                                        <input type="checkbox" checked
-                                               name="give_csv_toolbox_export_option[donation_date]"
-                                               id="give-export-donation-date"><?php esc_html_e( 'Donation Date', 'give' ); ?>
-                                    </label>
-                                </li>
-                                <li>
-                                    <label for="give-export-donation-time">
-                                        <input type="checkbox" checked
-                                               name="give_csv_toolbox_export_option[donation_time]"
-                                               id="give-export-donation-time"><?php esc_html_e( 'Donation Time', 'give' ); ?>
-                                    </label>
-                                </li>
+						<div id="give-csv-toolbox-standard-fields" class="give-clearfix">
+							<h4><?php esc_html_e( 'Standard Columns:', 'give' ); ?></h4>
+							<ul id="give-export-option-ul">
+								<li>
+									<label for="give-export-donation-id">
+										<input type="checkbox" checked
+											name="give_csv_toolbox_export_option[donation_id]"
+											id="give-export-donation-id"><?php esc_html_e( 'Donation ID', 'give' ); ?>
+									</label>
+								</li>
+								<li>
+									<label for="give-export-first-name">
+										<input type="checkbox" checked
+											name="give_csv_toolbox_export_option[first_name]"
+											id="give-export-first-name"><?php esc_html_e( 'Donor\'s First Name', 'give' ); ?>
+									</label>
+								</li>
+								<li>
+									<label for="give-export-last-name">
+										<input type="checkbox" checked
+											name="give_csv_toolbox_export_option[last_name]"
+											id="give-export-last-name"><?php esc_html_e( 'Donor\'s Last Name', 'give' ); ?>
+									</label>
+								</li>
+								<li>
+									<label for="give-export-email">
+										<input type="checkbox" checked
+											name="give_csv_toolbox_export_option[email]"
+											id="give-export-email"><?php esc_html_e( 'Donor\'s Email', 'give' ); ?>
+									</label>
+								</li>
+								<li>
+									<label for="give-export-address">
+										<input type="checkbox" checked
+											name="give_csv_toolbox_export_option[address]"
+											id="give-export-address"><?php esc_html_e( 'Donor\'s Billing Address', 'give' ); ?>
+									</label>
+								</li>
+								<li>
+									<label for="give-export-donation-sum">
+										<input type="checkbox" checked
+											name="give_csv_toolbox_export_option[donation_total]"
+											id="give-export-donation-sum"><?php esc_html_e( 'Donation Total', 'give' ); ?>
+									</label>
+								</li>
+								<li>
+									<label for="give-export-donation-status">
+										<input type="checkbox" checked
+											name="give_csv_toolbox_export_option[donation_status]"
+											id="give-export-donation-status"><?php esc_html_e( 'Donation Status', 'give' ); ?>
+									</label>
+								</li>
+								<!-- Add Custom Fields -->
+								<li>
+									<label for="give-export-give-simple-note-field">
+										<input type="checkbox" checked
+											name="give_csv_toolbox_export_option[give_simple_note_field]"
+											id="give-export-give-simple-note-field"><?php esc_html_e( 'Note', 'give' ); ?>
+									</label>
+								</li>
+								<li>
+									<label for="give-export-give-simple-anon-field">
+										<input type="checkbox" checked
+											name="give_csv_toolbox_export_option[give_simple_anon_field]"
+											id="give-export-give-simple-anon-field"><?php esc_html_e( 'Anonymous', 'give' ); ?>
+									</label>
+								</li>
+								<!-- END Add Custom Fields -->
+								<li>
+									<label for="give-export-payment-gateway">
+										<input type="checkbox" checked
+											name="give_csv_toolbox_export_option[payment_gateway]"
+											id="give-export-payment-gateway"><?php esc_html_e( 'Payment Gateway', 'give' ); ?>
+									</label>
+								</li>
+								<li>
+									<label for="give-export-donation-form-id">
+										<input type="checkbox" checked
+											name="give_csv_toolbox_export_option[form_id]"
+											id="give-export-donation-form-id"><?php esc_html_e( 'Donation Form ID', 'give' ); ?>
+									</label>
+								</li>
+								<li>
+									<label for="give-export-donation-form-title">
+										<input type="checkbox" checked
+											name="give_csv_toolbox_export_option[form_title]"
+											id="give-export-donation-form-title"><?php esc_html_e( 'Donation Form Title', 'give' ); ?>
+									</label>
+								</li>
+								<li>
+									<label for="give-export-donation-form-level-id">
+										<input type="checkbox" checked
+											name="give_csv_toolbox_export_option[form_level_id]"
+											id="give-export-donation-form-level-id"><?php esc_html_e( 'Donation Form Level ID', 'give' ); ?>
+									</label>
+								</li>
+								<li>
+									<label for="give-export-donation-form-level-title">
+										<input type="checkbox" checked
+											name="give_csv_toolbox_export_option[form_level_title]"
+											id="give-export-donation-form-level-title"><?php esc_html_e( 'Donation Form Level Title', 'give' ); ?>
+									</label>
+								</li>
+								<li>
+									<label for="give-export-donation-date">
+										<input type="checkbox" checked
+											name="give_csv_toolbox_export_option[donation_date]"
+											id="give-export-donation-date"><?php esc_html_e( 'Donation Date', 'give' ); ?>
+									</label>
+								</li>
+								<li>
+									<label for="give-export-donation-time">
+										<input type="checkbox" checked
+											name="give_csv_toolbox_export_option[donation_time]"
+											id="give-export-donation-time"><?php esc_html_e( 'Donation Time', 'give' ); ?>
+									</label>
+								</li>
 
-                                <li>
-                                    <label for="give-export-userid">
-                                        <input type="checkbox" checked
-                                               name="give_csv_toolbox_export_option[userid]"
-                                               id="give-export-userid"><?php esc_html_e( 'User ID', 'give' ); ?>
-                                    </label>
-                                </li>
-                                <li>
-                                    <label for="give-export-donorid">
-                                        <input type="checkbox" checked
-                                               name="give_csv_toolbox_export_option[donorid]"
-                                               id="give-export-donorid"><?php esc_html_e( 'Donor ID', 'give' ); ?>
-                                    </label>
-                                </li>
-                                <li>
-                                    <label for="give-export-donor-ip">
-                                        <input type="checkbox" checked
-                                               name="give_csv_toolbox_export_option[donor_ip]"
-                                               id="give-export-donor-ip"><?php esc_html_e( 'Donor IP Address', 'give' ); ?>
-                                    </label>
-                                </li>
-                            </ul>
-                        </div>
+								<li>
+									<label for="give-export-userid">
+										<input type="checkbox" checked
+											name="give_csv_toolbox_export_option[userid]"
+											id="give-export-userid"><?php esc_html_e( 'User ID', 'give' ); ?>
+									</label>
+								</li>
+								<li>
+									<label for="give-export-donorid">
+										<input type="checkbox" checked
+											name="give_csv_toolbox_export_option[donorid]"
+											id="give-export-donorid"><?php esc_html_e( 'Donor ID', 'give' ); ?>
+									</label>
+								</li>
+								<li>
+									<label for="give-export-donor-ip">
+										<input type="checkbox" checked
+											name="give_csv_toolbox_export_option[donor_ip]"
+											id="give-export-donor-ip"><?php esc_html_e( 'Donor IP Address', 'give' ); ?>
+									</label>
+								</li>
+							</ul>
+						</div>
 
-                        <div id="give-csv-toolbox-submit-wrap">
-                            <input type="hidden" name="give-export-class" value="Give_CSV_Toolbox_Donations_Export"/>
+						<div id="give-csv-toolbox-submit-wrap">
+							<input type="hidden" name="give-export-class" value="Give_CSV_Toolbox_Donations_Export"/>
 
-                            <input type="submit" value="<?php esc_attr_e( 'Generate CSV', 'give' ); ?>"
-                                   class="button button-primary"/>
-                            <span>
+							<input type="submit" value="<?php esc_attr_e( 'Generate CSV', 'give' ); ?>"
+								class="button button-primary"/>
+							<span>
 								<span class="spinner"></span>
 							</span>
 
-                        </div>
+						</div>
 
-                    </form>
-                </td>
-            </div>
+					</form>
+				</td>
+			</div>
 
 		<?php }
 
